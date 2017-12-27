e0ca61607a64a44402553c8fe35b30c06b06dd68
xiaoningboke <sunxiaoning@aliyun.com>
2017-12-27 09:53:16 +0800

    提交

---
+++ js/lunbo.js
@@ -0,0 +1,9 @@
+ $(function(){
+            $('#silder').imgSilder({
+				s_width:"350px", //容器宽度
+				s_height:"320px", //容器高度
+				is_showTit:true, // 是否显示图片标题 false :不显示，true :显示
+				s_times:3000, //设置滚动时间
+				css_link:'css/style.css'
+			});
+		});
